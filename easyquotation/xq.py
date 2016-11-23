2b5ee4811be6f7f6c97c73a4469d68e41c89994a
newctech <386510952@qq.com>
2016-11-23 23:45:34 +0800

    修复bug

--- easyquotation/xq.py
+++ easyquotation/xq.py
@@ -13,7 +13,7 @@ class Xueqiu:
 
     def __init__(self):
         self.headers = {
-            'Cookie': 'xq_a_token=xxxxxxxx;u=xxxxxxx,
+            'Cookie': 'xq_a_token=xxxxxxxx;u=xxxxxxx’,
             'User-Agent': 'Xueqiu Android 8.8',
             'Host': 'stock.xueqiu.com',
             'Pragma': 'no-cache',
