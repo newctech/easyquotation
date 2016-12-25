d05999e8709817de0eaef85e2a73a599394d139f
waverley <waverleytt@gmail.com>
2016-12-25 17:41:08 +0800

    修复bug

--- easyquotation/xq.py
+++ easyquotation/xq.py
@@ -299,7 +299,7 @@ class Xueqiu:
         yest_time = yesterday.strftime('%Y-%m-%d') + ' 15:00:00'
         today_time = time.strftime('%Y-%m-%d',time.localtime(time.time())) + ' 15:00:00'
         stock = self.get_kall_data(code, yest_time, today_time, autype, ktype, retry_count, pause)
-        if stock is not None:
+        if stock is not None and len(stock) > 0:
             if stock[-1] in self.__kstocks:
                 pass
             else:
